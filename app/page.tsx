da17de8b9c17758b619c5dd5cc4315325f3cdd9e
Tom Sherman <the.tomsherman@gmail.com>
2025-06-05 16:38:43 +0100

    Initial commit

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,15 @@
+import { redirect } from "next/navigation";
+
 /** Add your relevant code here for the issue to reproduce */
 export default function Home() {
-  return null;
+  return (
+    <form
+      action={async () => {
+        "use server";
+        redirect("mañana.com");
+      }}
+    >
+      <button>Go</button>
+    </form>
+  );
 }
